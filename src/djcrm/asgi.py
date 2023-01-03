c0bfee8601585feb4838a4a51e240206c6822c90
vaisakh <vaisakhsindhu11@gmail.com>
2023-01-03 09:37:18 +0530

    initial commit

--- src/djcrm/asgi.py
+++ src/djcrm/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for CRM project.
+ASGI config for djcrm project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CRM.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djcrm.settings')
 
 application = get_asgi_application()
